4e83ba41c71d8b30756bface0450c6ecf21eab86
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2022-01-18 08:30:55 +0500

    Admin+SU (#1)

--- library/urls.py
+++ library/urls.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from authors.views import AuthorModelViewSet
+from authapp.views import UserModelViewSet
 
 router = DefaultRouter()
 router.register('authors', AuthorModelViewSet)
+
+router1 = DefaultRouter()
+router1.register('authapp', UserModelViewSet)
 
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('api-auth/', include('rest_framework.urls')),
    path('api/', include(router.urls)),
+   path('api/', include(router1.urls)),
 ]
 
